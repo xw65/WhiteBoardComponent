a2d5c190bc47170c04d145f623ecf89f71e390bf
sean wang <xiaohang.wang.th@gmail.com>
2016-12-09 00:09:05 -0800

    push button change to master

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var hogan = require('hogan-express');
+
+// Express
+const app = express();
+app.engine('html', hogan);
+app.set('views', __dirname + '/view');
+app.use('/', express.static(__dirname));
+app.set('port', 8080);
+
+app.get('*', (req, res) => {
+    res.status(200).render('index.html');
+});
+
+app.listen(app.get('port'));
+
+console.info('==> ✅  Server is listening in ' + process.env.NODE_ENV + ' mode');
+console.info('==> 🌎  Go to http://localhost:%s', app.get('port'));
--- src/container.js
+++ src/container.js
@@ -1,1 +1,35 @@
+'use strict'
+
 // Container for text editor and white board.
+//const React =  require('react');
+//const ReactDOM = require('react-dom');
+
+class Container extends React.Component {
+    constructor() {
+        super();
+        this.state={
+          trick: "Click me or Lose!"
+        }
+    }
+
+    render() {
+      return(
+        <button onClick={() => this.setState({trick: this.props.trick})}>
+          {this.state.trick}
+        </button>
+        <div style={float: 'left'}>
+        </div>
+
+        <div style={flaot: 'right'}>
+        </div>
+      );
+    }
+}
+
+module.export = Container;
+
+let trick = "You Lose! Noooooob!!!";
+ReactDOM.render(
+  <Container trick={trick}/>,
+  document.getElementById('container')
+);
